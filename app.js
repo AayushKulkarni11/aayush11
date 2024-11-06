f82a01a79a90bd643c54fc5bf48768cf5074414c
AayushKulkarni11 <aayushkulkarni011@gmail.com>
2024-11-07 01:09:07 +0530

    add by aayush

---
+++ app.js
@@ -0,0 +1,112 @@
+// let pencilprice = 10;
+// let penprice = 20;
+// let output = `The total price is : ${pencilprice + penprice} Rupees.`;
+// console.log(output);
+
+// let color = "red";
+
+// if (color === "red"){
+//     console.log("stop");
+// }
+// if (color === "yellow") {
+//     console.log("go slow");
+// }
+// if (color === "green"){
+//     console.log("go");
+// }
+
+// let size = 'xl';
+
+// if(size === 'xl') {
+//     console.log(' Price is 250 rupees');
+// }else if(size ==='l'){
+//     console.log('Price is 200 rupees')
+// }else if(size === 'm')
+//     console.log('Price is 100 rupees')
+// }else (size ==='s'){
+//     console.log('Price is 50 rupees')
+// }
+
+// let str = 'apple';
+
+// if ( (str[0] === 'a') && (str.length > 3)){
+//     console.log('Yes! String is Good');
+// } else {('string is not good');
+// }
+
+// let num = 12;
+
+// if ( (num%3 === 0) && (num+1 === 15) || (num-1 === 11) ){
+//     console.log("safe");
+// } else{
+//     console.log("unsafe");
+// }
+
+// let day = 2;
+
+//  switch(day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     case 7:
+//         console.log("Sunday");
+//         break;
+//     default:
+//         console.log("Wrong")
+//  }
+
+
+// let a = "Aayush Kulkarni";
+
+// console.log(a.toUpperCase());
+// console.log(a.toLowerCase())
+
+// console.log(a.indexOf("K"));
+
+// console.log(a.slice(2,6));
+
+// console.log(a.replace("Aa", "Pi")); 
+
+// console.log(a.repeat(3));
+
+// let msg = "  Happy  ";
+
+// console.log(msg.trim().toUpperCase());
+
+let = a = "apnacollege";
+
+// console.log(a.slice(4).replace("l","t"));
+
+// let start = [ ' January','July','March','August'];
+
+// start.shift();
+// start.shift();
+// start.unshift('june');
+// start.unshift('july');
+
+// let a = [ 'January' , 'July' , 'March' , 'August'];
+
+// a.splice(0 , 2 , 'July' ,'June');
+
+// let a = [ ['X', '', 'O'], ['','X', ''], ['O','','X']];
+
+// for(let = i=5; i<=50; i+= 5 ) {
+//     console.log(i);
+// }
+
+// 2
